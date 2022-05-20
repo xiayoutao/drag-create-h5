4763dfd6242d7e704ca5a8196b0a13582a640ef1
xiayt <1425244634@qq.com>
2022-05-20 17:31:54 +0800

    perf: 优化

--- src/router/index.js
+++ src/router/index.js
@@ -2,42 +2,42 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { siteName } from '@/constants';
 
 const importPage = (view) => () => import(`@/views/${view}`);
-// const importFrame = view => () => import(`@/layouts/${view}`);
+const importFrame = (view) => () => import(`@/layouts/${view}`);
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		// {
-		//   path: '/',
-		//   redirect: '/home',
-		//   component: importFrame('Frame.vue'),
-		//   children: [
-		//     {
-		//       path: '/home',
-		//       component: importPage('home.vue'),
-		//     },
-		//     {
-		//       path: '/preview',
-		//       component: importPage('preview.vue'),
-		//     },
-		//     {
-		//       path: '/editor',
-		//       component: importPage('editor.vue'),
-		//     }
-		//   ]
-		// },
 		{
-			path: '/home',
-			component: importPage('home.vue'),
+			path: '/',
+			redirect: '/home',
+			component: importFrame('Frame.vue'),
+			children: [
+				{
+					path: '/home',
+					component: importPage('home.vue'),
+				},
+				{
+					path: '/preview',
+					component: importPage('preview.vue'),
+				},
+				{
+					path: '/editor',
+					component: importPage('editor.vue'),
+				},
+			],
 		},
+		// {
+		// 	path: '/home',
+		// 	component: importPage('home.vue'),
+		// },
 		{
 			path: '/preview',
 			component: importPage('preview.vue'),
 		},
-		{
-			path: '/editor',
-			component: importPage('editor.vue'),
-		},
+		// {
+		// 	path: '/editor',
+		// 	component: importPage('editor.vue'),
+		// },
 	],
 });
 
